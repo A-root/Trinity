274c9b3ea76ceb1eca827e60c6afc7a91f954efa
A-root <behemot666@tut.by>
2018-03-21 20:24:19 +0300

    change Enum

--- src/test/java/ru/giap/EnumModuleTest.java
+++ src/test/java/ru/giap/EnumModuleTest.java
@@ -1,9 +1,11 @@
 package ru.giap;
 
 import org.openqa.selenium.By;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -26,20 +28,56 @@ public class EnumModuleTest {
     public void createFolder() {
         title().contentEquals("Перечисления");
         $(By.cssSelector("[iddb='0']>.jstree-anchor")).contextClick();
-        $(By.xpath("//a[@rel=0]")).shouldBe(visible).click();
+        $(By.xpath("//a[@rel=2]")).shouldBe(visible).click();
         $(By.cssSelector(".jstree-rename-input")).setValue("Тестовая папка").pressEnter();
     }
 
     @Test(priority = 4)
-    public void createEnumInFilder() {
-//        $(By.cssSelector("a:contains('Тестовая папка')")).contextClick();
-        $(By.xpath("//a[@rel=2]")).shouldBe(visible).click();
-        $(By.xpath("//input[@name='name']")).setValue("Тестовое перечисление");
-        $(By.id("add-new")).click();
+    public void createEnumInFolder() {
+        $(By.cssSelector("[name='Тестовая папка']>.jstree-anchor")).contextClick();
+        $(By.xpath("//a[@rel=1]")).shouldBe(visible).click();
+        sleep(1000);
+        $(By.cssSelector("#new-enum-name")).setValue("Тестовое перечисление");
+        sleep(1000);
+        $(By.xpath("//div[@id='container-select-enum']//input[@type='text']")).setValue("Элемент1").pressEnter()
+                .setValue("Элемент2").pressEnter()
+                .setValue("Элемент3").pressEnter();
+        $(By.xpath("//button[@id='new-enum-btn']")).click();
     }
 
     @Test(priority = 5)
-    public void editEnum(){
+    public void editEnum() {
+//        $(By.cssSelector("[name='Тестовая папка'] > .jstree-ocl:nth-of-type(1)")).click();
+        sleep(3000);
+        $(By.cssSelector("[name='Тестовое перечисление'] > .jstree-ocl:nth-of-type(1)")).click();
+        //добавляем элемент в перечисление
+        $(By.id("addElEnum-textInput")).setValue("Элемент4");
+        $(By.id("def-main-ch")).click();
+        $(By.id("addElEnum-btn")).click();
+        //удаляем элемент из перечисления
+        $(By.id("enum-delete-choose")).click();
+        $(By.xpath("//form[@id='EditEnumContainer']/div/div[@class='col-md-2']//span[@class='box']")).click();
+        $(By.id("enum-delete-choose")).click();
+        $(By.cssSelector("#basicDanger .modal-body")).shouldBe(text("Удалить выбранные элементы перечисления?"));
+        $(By.id("deleteBtn")).click();
+    }
 
+    @Test(priority = 6)
+    public void deleteEnum() {
+        //удаляем перечисление
+        $(By.cssSelector("[name='Тестовое перечисление'] > .jstree-ocl:nth-of-type(1)")).contextClick();
+        $(By.xpath("//a[@rel=2]")).shouldBe(visible).click();
+        $(By.cssSelector("")).shouldHave(text("Вы действительно хотите удалить перечисление!"));
+        $(By.id("deleteBtn")).click();
+        //удаляем папку
+        $(By.cssSelector("[name='Тестовая папка'] > .jstree-ocl:nth-of-type(1)")).contextClick();
+        $(By.xpath("//a[@rel=2]")).shouldBe(visible).click();
+        $(By.cssSelector("")).shouldHave(text("Вы действительно хотите удалить папку!"));
+        $(By.id("deleteBtn")).click();
+    }
+
+    @AfterTest
+    public void tearDown(){
+        close();
     }
 }
